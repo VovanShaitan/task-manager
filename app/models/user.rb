8309bc8d3f384f0c72ee16961bf3ec5f7fef7c11
Vladimir-Fedorov <neroazzurri@mail.ru>
2022-10-15 21:41:28 +0400

    Added associations and validations for models "user" and "task"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+  
+  validates :first_name, presence: true, length: { minimum: 2, 
+    too_short: "%{count} characters is the minimum allowed" }
+  validates :last_name, presence: true, length: { minimum: 2, 
+    too_short: "%{count} characters is the minimum allowed" }
+  validates :email, presence: true, uniqueness: true, inclusion: { in: ['@'], 
+    message: "%{value} must contain the symbol '@'" }
 end
