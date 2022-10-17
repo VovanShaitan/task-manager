46cd786e492e80b9aae761f71a1d1d065675762e
Vladimir-Fedorov <neroazzurri@mail.ru>
2022-10-18 02:23:20 +0400

    Changed state for "Task" model in test

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     name
     description { "MyText" }
     author factory: :manager
-    assignee factory: :manager
-    state { "MyString" }
+    assignee factory: :developer
+    state { "new_task" }
     expired_at { "2022-10-12" }
   end
 end
